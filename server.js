0b494f271b8bb9ee1c09289ea920b9fc39da0d4f
jdwalden74 <jdwalden2@gmail.com>
2025-02-15 20:49:18 -0500

    fixed server again

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const {join} = require("node:path");
+const { join } = require("node:path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'public', 'login.html'));
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "public", "login.html"));
 });
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
-
