2aa6ea4722b175b23d8f2e8e26d28164bd463f84
jdwalden74 <jdwalden2@gmail.com>
2025-02-15 20:52:21 -0500

    fixed server again

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "public", "login.html"));
+    res.sendFile( join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
