64f2f1f28ee702eec47ceec51392b199b097e110
jdwalden74 <jdwalden2@gmail.com>
2025-02-15 20:47:16 -0500

    fixed server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const {join} = require("node:path");
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'login.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
+
